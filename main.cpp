701a393055094096050f434f9edbf400e62512b0
= <=forhadmethun@gmail.com>
2016-12-01 23:09:16 +0600

    11:08PM01:12:16

--- main.cpp
+++ main.cpp
@@ -2,41 +2,23 @@
 using namespace std;
 
 int main(){
-    //uri 1519
-    freopen("/home/forhadmethun/Documents/OJ/URI_BEGINNER/in.txt","r",stdin);
-    string str;
-    while(getline(cin,str)){
-        if(str == ".")break;
-        stringstream ss(str);
-        string temp;
-        vector<string> vs;
-        map<string, string> mss;
-        while(ss >> temp){
-            vs.push_back(temp);
+    //uri 2049
+    freopen("/home/forhadmethun/Documents/OJ/URI/in.txt","r",stdin);
+    string str1,str2;
+    int tc = 1;
+    //cout <<"test" <<endl;
+    while(getline(cin,str1)){
 
+        if(str1 == "0")break;
+        getline(cin,str2);
+        printf("Instancia %d\n",tc++);
+        cout << "------------------> " << str2.find(str1) << endl;
+        if(str2.find(str1) != string::npos){
+            cout << "verdadeira" << endl;
         }
-
-        for(auto it=vs.begin();it!=vs.end();it++){
-            if(it!=vs.begin()){
-                cout << " ";
-            }
-            if((*it).length() <=3){
-             cout << *it;
-            }
-            else{
-                string temp  = "";temp +=(*it)[0];
-                temp+=".";
-                cout << temp;// << ".";
-                mss[temp] = *it;
-
-            }
+        else{
+            cout << "falsa" << endl;
         }
-        cout <<endl  <<  mss.size() << endl;
-        for(auto it=mss.begin();it!=mss.end();it++){
-            cout << it->first  << " = " << it->second << endl;
-        }
-
-
         cout << endl;
 
 
